182faa26a28c73eef02a9c1430c0f12a642cf2ef
carlos julio <cjcs.cadenasarasty8@gmail.com>
2018-01-15 20:40:29 -0500

    se ajusto archivo php para crear los usuario, se creo consulta para verificar si el usuario y contraseña es correcto se crea metodo de conexión y desconexión

--- server/check_login.php
+++ server/check_login.php
@@ -1,6 +1,24 @@
 <?php
 
-
+    include 'conexionBD.php';
+    IniciarSesion();
+    function IniciarSesion(){
+       $UserName="Robert@msn.com";
+       $Password="123456";
+       IniciarConexion();
+       $Consulta="Select * from usuario where Username='".$UserName."'";
+       $Resultado= ($GLOBALS['Conexion']->query($Consulta));
+       while ($fila = mysqli_fetch_array($Resultado)){
+            if(password_verify($Password,$fila['Password'])){
+                echo "Inicio Sesión Resultado: Exitoso";
+            }
+            else{
+                echo "Inicio Sesión Resultado: Fallido";
+            }
+        }
+       
+       DesactivarConexion();
+    }
 
 
  ?>
