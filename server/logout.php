54a313b6708ed25b78b8c28d4f76c022d67a7a39
carlos julio <cjcs.cadenasarasty8@gmail.com>
2018-01-16 22:38:32 -0500

    se elimino archivos netbean

--- server/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'check_login.php';
-    $IdUser=0;
-    $NombreUsuario='';
-    setcookie('IdUser',$IdUser);
-    setcookie('Nombre',$Nombre);
-    header('Location: '.'../client/index.html');
- ?>
