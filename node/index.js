bf32d62349395ee9dd51045a7c27e73905618a99
carlos julio <cjcs.cadenasarasty8@gmail.com>
2018-01-22 23:37:03 -0500

    se crea mediante script la tabla y un usuario se valida que no se crea multiples veces se valida el inicio de sesión

--- node/index.js
+++ node/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var body_parser = require('body-parser');
-var path = require("path");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/examen');
+var Schema = mongoose.Schema;
+var child = new Schema({ name: String });
+var userSchemaJson={
+  email:String,
+  password:String
+};
+var userSchema=new Schema(userSchemaJson);
+var UserModelo=mongoose.model("User",userSchema);
 var app = express();
 
 
@@ -10,9 +19,37 @@ app.use(body_parser.urlencoded({extended:true}));
 app.post("/login",function(req,res){
   var user=req.body.user || '';
   var pass=req.body.pass || '';
-  res.end(user+" "+pass);
+  UserModelo.findOne({email:user}).exec(function(err,docs){
+    if(docs!=null){
+      if(pass.search(docs.password)!=-1){
+      res.end("Validado");
+      }else{
+          res.end("Usuario o contraseña incorrecta");
+      }
+    }else{
+      res.end("Usuario o contraseña incorrecta");
+    }
+  })
+//  res.end(user+" "+pass);
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Escuchando por el puerto 3000');
+  CrearUsuarios("cjcs@dominio.com","123456");
 });
+
+
+function CrearUsuarios(user,pass){
+  UserModelo.findOne({email:user}).exec(function(err,docs){
+    if(docs==null){
+      var usuario=new UserModelo({email:user,password:pass});
+      usuario.save();
+    }
+  });
+}
+
+
+function DesconectarBase(){
+  mongoose.disconnect();
+  module.exports=mongoose;
+}
